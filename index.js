cd317dc184187362bce493f1f31ac34719716d68
wento <florea_andreiwento@yahoo.com>
2023-02-14 00:14:05 -0600

    added new repo

---
+++ index.js
@@ -0,0 +1,178 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown.js');
+
+
+// TODO: Create an array of questions for user input
+const questions = [
+
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+        validate: input => {
+            if (input) {
+                return true;
+            } else {
+                console.log('Please enter a title!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: "What is your project's description?",
+        validate: input => {
+            if (input) {
+                return true;
+            } else {
+                console.log('Please enter a description!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'confirm',
+        name: 'confirmTable',
+        message: 'Do you want a Table of Contents?',
+        default: true
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What are your installation instructions?',
+        validate: input => {
+            if (input) {
+                return true;
+            } else {
+                console.log('Please enter any info on how to install!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'What is your usage information?',
+        validate: input => {
+            if (input) {
+                return true;
+            } else {
+                console.log('Please enter any info for usage!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'What License applies to your application?',
+        choices: [
+
+
+            'None',
+            'Attribution-ShareAlike 4.0 International',
+            'Eclipse Public License 1.0',
+            'IBM Public License Version 1.0',
+            'The MIT License',
+            'Mozilla Public License 2.0',
+            'The Perl License',
+
+
+        ]        
+    },
+    {
+        type: 'input',
+        name: 'contribute',
+        message: "What are your contribution guidelines?",
+        validate: input => {
+            if (input) {
+                return true;
+            } else {
+                console.log('Please enter any how to contribute info!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'What are your test instructions?',
+        validate: input => {
+            if (input) {
+                return true;
+            } else {
+                console.log('Please enter any how to test info!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'username',
+        message: "What is your GitHub Username?",
+        validate: input => {
+            if (input) {
+                return true;
+            } else {
+                console.log('Please enter a username!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: "What is your email address?",
+        validate: input => {
+            if (input) {
+                return true;
+            } else {
+                console.log('Please enter a email!');
+                return false;
+            }
+        }
+    },
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+    return new Promise((resolve, reject) => {
+        fs.writeFile(fileName, data, error => {
+            if (error) {
+                reject(error);
+                return;
+            }
+
+            resolve({
+                ok: true,
+                message: 'File created!'
+            });
+        });
+    });
+};
+
+
+
+
+// TODO: Create a function to initialize app
+// function init() {}
+function init() {
+    inquirer.prompt(questions)
+        .then(answers => {
+            console.log(answers);
+            return generateMarkdown(answers);
+        })
+        .then(pageMarkdown => {
+            writeToFile('./dist/README.md', pageMarkdown);
+            console.log('README.md created!')
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+}
+
+// Function call to initialize app
+init();
---
+++ utils/licensesDetails.js
@@ -0,0 +1,43 @@
+const licensesDetails = [
+
+    {
+        'name':'Attribution-ShareAlike 4.0 International',
+        'badge':`[![License: CC BY-SA 4.0](https://img.shields.io/badge/License-CC_BY--SA_4.0-lightgrey.svg)]`,
+        'link':`(https://creativecommons.org/licenses/by-sa/4.0/)`
+
+    },
+
+
+    {
+        'name': 'Eclipse Public License 1.0',
+
+        'badge':`[![License](https://img.shields.io/badge/License-EPL_1.0-red.svg)]`,
+        'link':`(https://opensource.org/licenses/EPL-1.0)`
+    },
+
+    {
+        'name':'IBM Public License Version 1.0',
+        'badge':`[![License: IPL 1.0](https://img.shields.io/badge/License-IPL_1.0-blue.svg)]`,
+        'link':`(https://opensource.org/licenses/IPL-1.0)`
+    },
+
+    {
+        'name':'The MIT License',
+        'badge':`[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)]`,
+        'link':`(https://opensource.org/licenses/MIT)`
+    },
+
+    {
+        'name':'Mozilla Public License 2.0',
+        'badge':`[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)]`,
+        'link':`(https://opensource.org/licenses/MPL-2.0)`
+    },
+
+    {
+        'name':'The Perl License',
+        'badge':`[![License: Artistic-2.0](https://img.shields.io/badge/License-Perl-0298c3.svg)]`,
+        'link':`https://opensource.org/licenses/Artistic-2.0)`
+    }
+]
+
+module.exports = licensesDetails;
